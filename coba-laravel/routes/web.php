d16b0a9d844edecf93405e82e6b28ffafc7b777b
tia sasmita <sasmitatia74@gmail.com>
2023-07-16 19:47:14 +0700

    model,collectin,controller Eps 5_ database,migration,eloquent Eps 6

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,55 +32,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Posts Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rini Sasmita",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo impedit delectus cumque iure fuga quibusdam dolor cum omnis modi itaque quo ea et maxime laboriosam aspernatur nobis ipsam blanditiis sequi odio, labore culpa veniam optio? Vero ratione quasi hic impedit! Enim itaque dolorum consequuntur atque modi, praesentium esse, fuga dignissimos, quisquam perspiciatis commodi rerum voluptas quae facilis adipisci ipsa labore? Optio tempora voluptatem animi reprehenderit cupiditate enim sed laudantium non temporibus obcaecati quidem, dolores soluta ex ab exercitationem nisi repudiandae!"
-        ],
-        [
-            "title" => "Judul Posts Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tia Sasmita",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, numquam repudiandae soluta enim quas iure molestiae magnam aspernatur id accusantium! Expedita eum molestiae fugiat porro, magni dolores blanditiis laboriosam itaque consequuntur distinctio tenetur commodi iste recusandae impedit sequi voluptas voluptates voluptatum nisi cupiditate dignissimos dolorem neque. Iure hic, exercitationem, sed dolore quia tenetur odio itaque ullam laborum unde accusantium, excepturi porro blanditiis! Nobis, laborum saepe perferendis voluptates architecto rem, amet laudantium quia quis non autem quae sed vel ipsum illum maxime neque tenetur omnis molestiae reprehenderit iste! Quo reiciendis tempore delectus laborum commodi, minus, mollitia accusamus quasi ipsum quae quis."
-        ]
-    ];
-    return view('posts',[
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-
-
+Route::get('/blog', [PostController::class, 'index']);
 // Halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Posts Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Rini Sasmita",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo impedit delectus cumque iure fuga quibusdam dolor cum omnis modi itaque quo ea et maxime laboriosam aspernatur nobis ipsam blanditiis sequi odio, labore culpa veniam optio? Vero ratione quasi hic impedit! Enim itaque dolorum consequuntur atque modi, praesentium esse, fuga dignissimos, quisquam perspiciatis commodi rerum voluptas quae facilis adipisci ipsa labore? Optio tempora voluptatem animi reprehenderit cupiditate enim sed laudantium non temporibus obcaecati quidem, dolores soluta ex ab exercitationem nisi repudiandae!"
-        ],
-        [
-            "title" => "Judul Posts Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Tia Sasmita",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, numquam repudiandae soluta enim quas iure molestiae magnam aspernatur id accusantium! Expedita eum molestiae fugiat porro, magni dolores blanditiis laboriosam itaque consequuntur distinctio tenetur commodi iste recusandae impedit sequi voluptas voluptates voluptatum nisi cupiditate dignissimos dolorem neque. Iure hic, exercitationem, sed dolore quia tenetur odio itaque ullam laborum unde accusantium, excepturi porro blanditiis! Nobis, laborum saepe perferendis voluptates architecto rem, amet laudantium quia quis non autem quae sed vel ipsum illum maxime neque tenetur omnis molestiae reprehenderit iste! Quo reiciendis tempore delectus laborum commodi, minus, mollitia accusamus quasi ipsum quae quis."
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
