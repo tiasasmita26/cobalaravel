b81a652a3f8ebb5d18233607a7348cb508c2ebfa
tia sasmita <sasmitatia74@gmail.com>
2023-07-20 22:00:35 +0700

    authorization

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 
 // use App\Models\Post;
 use App\Models\Category;
@@ -68,6 +69,8 @@ Route::get('/dashboard', function(){
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
 
 // Route::get('/categories/{category:slug}', function(Category $category){
 //     return view('posts', [
